99b40ea588b911f364bfa12d0efa41e05d1e9d30
marcocola2001 <cola01.mc@gmail.com>
2023-05-12 14:44:43 +0200

    Esercitazione settimana 5 fatta

--- Preparazione nuova (Estate 2023)/Pile e code/Esercizi pila 5a settimana/es1.1_pilaPostfix.go
+++ Preparazione nuova (Estate 2023)/Pile e code/Esercizi pila 5a settimana/es1.1_pilaPostfix.go
@@ -5,16 +5,18 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"unicode"
 )
 
+// Pila implementata come slice di interi dove pila[len(pila)-1] = cima della pila
 func valuta(str string) int {
 	pila := make([]int, 0)
 	for i := 0; i < len(str); i++ {
-		if unicode.isDigit(str[i]) {
-			n, _ := strconv.Atoi(str[i])
-			pila = append(pila, n)
-		} else {
-			n1, n2 := pila[len(pila)-1], pila[len(pila)-2]
+		if unicode.IsDigit(rune(str[i])) {
+			n, _ := strconv.Atoi(string(str[i]))
+			pila = append(pila, n) //Push sulla pila
+		} else if str[i] != ' ' {
+			n1, n2 := pila[len(pila)-1], pila[len(pila)-2] //Pop sulla pila x2
 			pila = pila[:len(pila)-2]
 			switch str[i] {
 			case '+':
